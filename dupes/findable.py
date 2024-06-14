90b78cd5e7cd7a87ff2c2dabe88851c2436c45cd
mivanoffka <108406837+mivanoffka@users.noreply.github.com>
2024-06-14 13:06:37 +0300

    Initial commit
    Abstract duplicates-finder class and class for image folders are created

---
+++ dupes/findable.py
@@ -0,0 +1,24 @@
+from abc import ABCMeta, abstractmethod
+from pathlib import Path
+
+from dupes.image_folder import ImageFolder
+
+
+class Findable(meta=ABCMeta):
+    @abstractmethod
+    def _find_duplicates_for_single_folder(self, image_folder: ImageFolder):
+        raise NotImplementedError
+
+    @abstractmethod
+    def _find_duplicates_for_two_folders(self, first_image_folder: ImageFolder, second_image_folder: ImageFolder):
+        raise NotImplementedError
+
+    def find_duplicates(self, *image_folders: ImageFolder):
+        if len(image_folders) == 1:
+            return self._find_duplicates_for_single_folder(image_folders[0])
+        elif len(image_folders) == 2:
+            return self._find_duplicates_for_two_folders(*image_folders)
+        elif len(image_folders) == 0:
+            raise ValueError("No image folders were given. Please provide one or two folders.")
+        else:
+            raise ValueError("Too many image folders were given. Please provide one or two folders.")
---
+++ dupes/image_folder.py
@@ -0,0 +1,49 @@
+from pathlib import Path, PosixPath
+from typing import Optional, Iterable
+
+
+class ImageFolder:
+    _folder_path: str
+    _allowed_file_formats: tuple[str] = ["*.jpg", "*.jpeg"]
+    _file_formats_filter = Optional[list[str]]
+
+    @property
+    def folder_path(self):
+        return self._folder_path
+
+    @property
+    def selected_file_formats(self) -> tuple[str]:
+        selected_file_formats = []
+        if self._file_formats_filter is None:
+            selected_file_formats = self._allowed_file_formats
+        else:
+            for file_format in self._file_formats_filter:
+                if file_format in self._allowed_file_formats:
+                    selected_file_formats.append(file_format)
+
+                else:
+                    raise AttributeError(f"Invalid file format '{file_format}'."
+                                         f" Valid formats are: {self._allowed_file_formats}")
+
+        return tuple(selected_file_formats)
+
+    @property
+    def file_paths(self) -> tuple[Path, ...]:
+        file_paths = []
+
+        for file_format in self.selected_file_formats:
+            file_paths.extend(list(Path(self._folder_path).glob(file_format)))
+
+        return tuple(file_paths)
+
+    def __init__(self, folder_path: str, file_formats_filter: Optional[Iterable[str]] = None):
+        self._folder_path = folder_path
+        self._file_formats_filter = file_formats_filter
+
+
+if __name__ == "__main__":
+    img_folder = ImageFolder("/Users/mivanoffka/Pictures/df_test/1")
+    print(img_folder.selected_file_formats)
+    print(img_folder.folder_path)
+    print(img_folder.file_paths)
+
